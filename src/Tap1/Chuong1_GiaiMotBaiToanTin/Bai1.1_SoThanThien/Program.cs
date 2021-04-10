33c7c975ba432dd793b769d57f5f5e80d79185c2
La Quốc Thắng <quocthang0507@gmail.com>
2021-04-10 19:38:19 +0700

    Add proj 1.1
    Signed-off-by: La Quốc Thắng <quocthang0507@gmail.com>

---
+++ src/Tap1/Chuong1_GiaiMotBaiToanTin/Bai1.1_SoThanThien/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Bai1._1_SoThanThien
+{
+	class Program
+	{
+		static int mn = 90;
+		static int[] s = new int[mn];
+
+		static void Main(string[] args)
+		{
+			Run();
+			Console.ReadKey();
+		}
+
+		private static void Run()
+		{
+			int n = Find();
+			for (int i = 0; i < n; i++)
+			{
+				Console.Write(s[i] + " ");
+			}
+			Console.WriteLine($"Tong cong: {n} so");
+		}
+
+		private static int Find()
+		{
+			int d = 0;
+			for (int x = 10; x < 100; x++)
+			{
+				if (UCLN(x, SoDao(x)) == 1)
+					s[d++] = x;
+			}
+			return d;
+		}
+
+		private static int SoDao(int x)
+		{
+			int y = 0;
+			do
+			{
+				y = y * 10 + (x % 10);
+				x /= 10;
+			} while (x != 0);
+			return y;
+		}
+
+		private static int UCLN(int a, int b)
+		{
+			int r;
+			while (b != 0)
+			{
+				r = a % b;
+				a = b;
+				b = r;
+			}
+			return a;
+		}
+	}
+}
---
+++ src/Tap1/Chuong1_GiaiMotBaiToanTin/Bai1.1_SoThanThien/README.md
@@ -0,0 +1,21 @@
+﻿# 1.1. Số thân thiện
+
+*Tìm tất cả các số tự nhiên hai chữ số mà khi đảo trật tự của hai chữ số đó sẽ thu được một số nguyên tố cùng nhau với số đã cho.*
+
+## Hiểu đầu bài
+
+Ta kí hiệu (a, b) là ước chung lớn nhất (ucln) của hai số tự nhiên a và b. Hai số tự nhiên a và b được gọi là nguyên tố cùng nhau khi và chỉ khi (a, b) = 1.
+
+Ví dụ:
+
+``` c#
+(23, 32) = 1 // Vậy 23 và 32 là một số cần tìm
+
+(12, 21) = 3 // 12 và 21 không phải là một số cần tìm
+```
+
+**Đặc tả:**
+(1) x = ab
+(2) a, b = 0..9 (a và b biến thiên trong khoảng 0..9)
+(3) a > 0 vì x là số có hai chữ số
+(4) (ab, ba) = 1
