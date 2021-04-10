8a180797a569109d187429473df2cdaffb6ee043
La Quốc Thắng <quocthang0507@gmail.com>
2021-04-10 20:09:49 +0700

    Add proj 1.2
    Signed-off-by: La Quốc Thắng <quocthang0507@gmail.com>

---
+++ src/Tap1/Chuong1_GiaiMotBaiToanTin/Bai1.2_SoCapCong/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Bai1._2_SoCapCong
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Show(Find());
+			Console.WriteLine("\nHoan thanh");
+			Show(Find2());
+			Console.ReadKey();
+		}
+
+		static void Show(int[] s)
+		{
+			foreach (var x in s)
+			{
+				Console.Write(x + " ");
+			}
+		}
+
+		static int[] Find()
+		{
+			int d = 0, x;
+			int[] ChuSoLe = { 1, 3, 5, 7, 9 },
+			s = new int[25];
+			for (int i = 0; i < 5; i++)
+			{
+				x = 105 * ChuSoLe[i];
+				for (int j = 0; j < 5; j++)
+				{
+					s[d++] = x + 6 * ChuSoLe[j];
+				}
+			}
+			return s;
+		}
+
+		static int[] Find2()
+		{
+			int d = 0;
+			int[] s = new int[25];
+			for (int a = 1; a <= 9; a++)
+			{
+				for (int b = 0; b <= 9; b++)
+				{
+					for (int c = 0; c <= 9; c++)
+					{
+						if(c %2!=0 && (2 * b == a + c))
+						{
+							s[d++] = 100 * a + 10 * b + c;
+						}
+					}
+				}
+			}
+			return s;
+		}
+	}
+}
---
+++ src/Tap1/Chuong1_GiaiMotBaiToanTin/Bai1.2_SoCapCong/README.md
@@ -0,0 +1,25 @@
+﻿# Số cấp cộng
+
+*Tìm các số tự nhiên lẻ có ba chữ số. Ba chữ số này, theo trật tự từ trái qua phải tạo thành một cấp số cộng*
+
+## Đặc tả:
+
+(1) x là số tự nhiên có ba chữ số: x = 100*a + 10*b + c
+
+(2) x là số lẻ nên chữ số hàng đơn vị c là số lẻ: c = 1, 3, 5, 7, 9
+
+(3) Chữ số hàng trăm của x phải khác 0: a = 1..9
+
+(4) Nếu dãy a, b, c lập thành một cấp số cộng thì số đứng giữa b là trung bình cộng của hai số đầu và cuối: b = (a + c)/2 hay 2b = a + c
+
+Từ (4) suy ra (a + c) là số chẵn. Do c lẻ, (a + c) chẵn nên a lẻ.
+
+Nếu biết a và c thì ta tính được:
+
+```c#
+x = 100a + 10(a + c)/2 + c
+= 100a + 5(a + c) + c
+= 105a + 6c
+```
+
+Vì có 5 chữ số lẻ nên tổ hợp của a và c sẽ cho ta 25 số.
