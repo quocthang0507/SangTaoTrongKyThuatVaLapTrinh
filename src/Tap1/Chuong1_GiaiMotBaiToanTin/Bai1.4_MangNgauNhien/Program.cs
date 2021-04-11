d37894ec4f2cfd1603b70bcb7b51bf2926afafa6
La Quốc Thắng <quocthang0507@gmail.com>
2021-04-11 18:57:45 +0700

    Add new proj 1.4

---
+++ src/Tap1/Chuong1_GiaiMotBaiToanTin/Bai1.4_MangNgauNhien/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+
+namespace Bai1._4_MangNgauNhien
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// Show(Gen(200));
+			ShowStat(Stat(Gen(30)));
+			Console.WriteLine("\nFinish");
+			Console.ReadKey();
+		}
+
+		private static int[] Gen(int n)
+		{
+			int[] a = new int[n];
+			Random r = new Random();
+			for (int i = 0; i < n; i++)
+			{
+				a[i] = r.Next(n);
+			}
+			return a;
+		}
+
+		static void Show(int[] list)
+		{
+			foreach (var n in list)
+			{
+				Console.Write(n + " ");
+			}
+			Console.WriteLine();
+		}
+
+		static int[] Stat(int[] list)
+		{
+			int[] a = new int[list.Length];
+			for (int i = 0; i < a.Length; i++)
+			{
+				a[i] = 0;
+			}
+			for (int n = 0; n < list.Length; n++)
+			{
+				a[list[n]]++;
+			}
+			return a;
+		}
+
+		static void ShowStat(int[] list)
+		{
+			for (int i = 0; i < list.Length; i++)
+			{
+				Console.WriteLine($"{i} xuat hien {list[i]} lan");
+			}
+		}
+	}
+}
---
+++ src/Tap1/Chuong1_GiaiMotBaiToanTin/Bai1.4_MangNgauNhien/README.md
@@ -0,0 +1,23 @@
+﻿# Mảng ngẫu nhiên
+
+*Sinh ngẫu nhiên n số nguyên không âm cho mảng nguyên a*
+
+## Đặc tả
+
+Trong Turbo Pascal, hàm `random(n)` sinh một số ngẫu nhiên kiểu nguyên nằm trong khoảng từ 0 đến n-1.
+
+Trong C#, phương thức `Next(n)` của lớp `Random` hoạt động tương tự.
+
+## Chú ý
+
+(1) Trước khi gọi hàm `random` ta cần gọi thủ tục `randomize` để máy tính khởi động cơ chế phát sinh số ngẫu nhiên.
+
+(2) Thủ tục `Gen(m)` trong chương trình dưới đây sinh số ngẫu nhiên m số nguyên trong khoảng từ 0 đến m-1. Ta có thể cải tiến để viết thủ tục Gen(n, d, c) để sinh ngẫu nhiên n số nguyên dương trong khoảng từ d đến c (d < c).
+
+Để ý rằng `random(c-d+1)` biến thiên trong khoảng từ 0 đến c-d, do đó `d + random(c-d+1)` sẽ biến thiên trong khoảng từ d đến `d+c-d = c`
+
+## Kết quả
+
+```
+
+```
