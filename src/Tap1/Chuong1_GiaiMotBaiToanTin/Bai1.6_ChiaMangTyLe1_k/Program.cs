595103eef8c8289833fd795a27c3658cbaf4abd1
La Quốc Thắng <quocthang0507@gmail.com>
2021-04-18 19:08:48 +0700

    Upload proj 1.6
    Signed-off-by: La Quốc Thắng <quocthang0507@gmail.com>

---
+++ src/Tap1/Chuong1_GiaiMotBaiToanTin/Bai1.6_ChiaMangTyLe1_k/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+
+namespace Bai1._6_ChiaMangTyLe1_k
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			do
+			{
+				Run(10, 3);
+				Console.Write("\nBam phim bat ky de tiep tuc, bam phim T de thoat: ");
+			} while (Console.ReadLine() != "T");
+		}
+
+		private static void Run(int n, int k)
+		{
+			if (n < 0 || n > 1000000 || k < 1)
+				return;
+			int[] a = Gen(n, k);
+			Print(a);
+			int d = Chia(a, k);
+			if (d < 0)
+				Console.WriteLine("\nVo nghiem");
+			else
+				Console.WriteLine("\n" + Test(a, d, k));
+		}
+
+		/// <summary>
+		/// Kiem tra k * sum(a[1..d]) = sum(a[d+1..n])?
+		/// </summary>
+		/// <param name="a"></param>
+		/// <param name="d"></param>
+		/// <param name="k"></param>
+		/// <returns></returns>
+		private static bool Test(int[] a, int d, int k)
+		{
+			Console.WriteLine($"\n\nTest, k = {k}, diem chia = {d}");
+			int t1 = 0, t2 = 0;
+			for (int i = 0; i < d; i++)
+			{
+				t1 += a[i];
+			}
+			for (int i = d; i < a.Length; i++)
+			{
+				t2 += a[i];
+			}
+			Console.WriteLine($"Sum1 = {t1}, sum2 = {t2}");
+			return (t1 == k * t2 || t2 == k * t1);
+		}
+
+		private static int Chia(int[] a, int k)
+		{
+			int t = 0;
+			foreach (var x in a)
+			{
+				t += x;
+			}
+			if (t % (k + 1) != 0)
+				return -1;
+			int t1 = t / (k + 1), t2 = t - t1, tr = 0;
+			for (int i = 0; i < a.Length; i++)
+			{
+				tr += a[i];
+				if (tr == t1 || tr == t2)
+					return i + 1;
+			}
+			return -1;
+		}
+
+		private static void Print(int[] a)
+		{
+			Console.WriteLine();
+			Console.WriteLine(new string('=', a.Length * 5));
+			foreach (var x in a)
+			{
+				Console.Write($"{x}    ");
+			}
+			Console.WriteLine();
+		}
+
+		private static int[] Gen(int n, int k)
+		{
+			Random random = new Random();
+			int[] a = new int[n];
+			if (random.Next(2) == 0)
+			{
+				// Khoang 1/2 so test la vo nghiem
+				for (int i = 0; i < n; i++)
+					a[i] = random.Next(n);
+			}
+			int d = random.Next(n / 2) + 1; // Diem chia
+			int t = 0, d1 = d - 1;
+			for (int i = 0; i < d1; i++)
+			{
+				a[i] = random.Next(n);
+				t += a[i];
+			}
+			if (random.Next(2) == 0)
+				// Doan dau a[1..d] gap k lan doan cuoi a[d+1..n]
+				a[d1] += (k - 1) * t;
+			else
+				// Doan cuoi gap k lan doan dau
+				t *= k;
+			int n1 = n - 1;
+			for (int i = d; i < n1; i++)
+			{
+				a[i] = random.Next(t);
+				t -= a[i];
+			}
+			a[n1] = t;
+			return a;
+		}
+	}
+}
---
+++ src/Tap1/Chuong1_GiaiMotBaiToanTin/Bai1.6_ChiaMangTyLe1_k/README.md
@@ -0,0 +1,68 @@
+﻿# Chia mảng tỷ lệ 1:k
+
+*Tìm cách chia dãy số nguyên không âm a1, a2,...,an, n > 1 cho trước thành hai đoạn có tổng các phần tử trong một đoạn gấp k lần tổng các phần tử trong đoạn kia, k nguyên dương.*
+
+## Đặc tả
+
+Gọi t là tổng các phần tử của dãy a, `t = sum(a[1..n])`
+
+Muốn chia a thành hai đoạn a[1..i] và a[i+1..n] có tổng gấp nhau k lần, ta phải có:
+
+(1) t chia hết cho (k+1). Đặt `t1 = t div (k+1)` và tk = t - t1
+
+(2) `(#E i: 1 <= i <= n): sum(a[1..i]) = t1` hoặc `sum(a[1..i]) = tk`
+
+Để ý rằng nếu k = 1 thì t1 = tk, nếu k > 1 thì t1 < tk, do đó bài này là trường hợp riêng của bài trước khi k = 1
+
+Trong chương trình dưới đây, hàm `Chia(k)` cho giá trị i nếu mảng a chia được
+thành hai đoạn a[1..i] và a[(i + 1)..n] có tổng gấp k lần nhau. Trong trường hợp vô
+nghiệm `Chia = -1`. Ta gọi i là điểm chia và dùng biến tr (tổng riêng) để tích luỹ
+tổng các phần tử của đoạn đang xét a[1..i]. Khi tr = t1 bài toán có nghiệm I, ngược lại,
+khi tr > t1 ta chưa thể kết luận là bài toán vô nghiệm. Trường hợp này ta phải tiếp tục
+tích luỹ tr để hi vọng đạt được tổng tr = tk. Nếu sau khi tích luỹ ta thu được tr = tk thì
+bài toán có nghiệm i, ngược lại, khi tr > tk ta kết luận là bài toán vô nghiệm.
+
+Ta gọi thủ tục Gen để sinh dữ liệu kiểm thử. Cũng giống như bài trước, ta sẽ sinh
+ngẫu nhiên dữ liệu kiểm thử cho hai trường hợp: chắc chắn có nghiệm và có thể vô
+nghiệm. Với trường hợp có thể vô nghiệm ta sinh ngẫu nhiên như bình thường:
+
+```c#
+for i := 1 to n do a[i] := random(n);
+```
+
+Với trường hợp có nghiệm, ta sinh ngẫu nhiên mảng a gồm hai đoạn:
+Đoạn thứ nhất a[1..d] và đoạn thứ hai a[d + 1..n] trong đó d là một điểm chia được sinh ngẫu nhiên
+```c#
+d := random(n div 2)+1; {diem chia}
+```
+
+Ta lại chọn ngẫu nhiên một trong hai trường hợp:
+* Trường hợp thứ nhất: đoạn thứ nhất gấp k lần đoạn thứ hai.
+* Trường hợp thứ hai: đoạn thứ hai gấp k lần đoạn thứ nhất.
+
+## Kết quả
+
+```
+==================================================
+0    0    0    0    0    0    0    0    0    0
+
+
+Test, k = 3, diem chia = 1
+Sum1 = 0, sum2 = 0
+```
+
+```
+==================================================
+6    7    5    36    8    6    2    1    0    1
+
+
+Test, k = 3, diem chia = 3
+Sum1 = 18, sum2 = 54
+```
+
+```
+==================================================
+2    10    0    1    0    0    0    0    0    1
+
+Vo nghiem
+```
