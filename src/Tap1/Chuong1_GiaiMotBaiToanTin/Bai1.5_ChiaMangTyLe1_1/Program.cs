1df078caf2a76242099dbae4b1690e653056f2e8
La Quốc Thắng <quocthang0507@gmail.com>
2021-04-18 17:36:07 +0700

    Add proj 1.5
    Signed-off-by: La Quốc Thắng <quocthang0507@gmail.com>

---
+++ src/Tap1/Chuong1_GiaiMotBaiToanTin/Bai1.5_ChiaMangTyLe1_1/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+
+namespace Bai1._5_ChiaMangTyLe1_1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			do
+			{
+				Run(10);
+				Console.Write("\nBam phim Enter de tiep tuc, bam phim T de thoat: ");
+			} while (Console.ReadLine() == "");
+		}
+
+		private static void Run(int n)
+		{
+			int[] a = new int[n];
+			Gen(a, n);
+			Print(a, n);
+			int t = 0, d = Chia(a, n, ref t);
+			if (d < 0)
+				Console.WriteLine("\nKhong chia duoc");
+			else if (KiemTra(a, n, d))
+			{
+				Console.WriteLine("\nDoan thu nhat: 1..{0} ", d);
+				Console.WriteLine("\nDoan thu hai: {0}..{1} ", d + 1, n);
+			}
+			else
+				Console.WriteLine("\nLoi giai sai!");
+		}
+
+		/// <summary>
+		/// Kiem tra sum(a[1..d]) == sum(a[d+1..n])
+		/// </summary>
+		/// <param name="a"></param>
+		/// <param name="n"></param>
+		/// <param name="d"></param>
+		/// <returns></returns>
+		private static bool KiemTra(int[] a, int n, int d)
+		{
+			if (d < 0 || d >= n)
+				return false;
+			int t = 0;
+			for (int i = 0; i < d; i++)
+				t += a[i];
+			for (int i = d; i < n; i++)
+				t -= a[i];
+			return t == 0;
+		}
+
+		private static int Chia(int[] a, int n, ref int t)
+		{
+			int sum = 0; // sum = tong(a[1..n])
+			for (int i = 0; i < n; i++)
+				sum += a[i];
+			if (sum % 2 != 0)
+				return -1;
+			t = sum / 2;
+			int tr = 0;
+			for (int i = 0; i < n; i++)
+			{
+				tr += a[i];
+				if (tr == t)
+					return i + 1;
+			}
+			return -1;
+		}
+
+		private static void Print(int[] a, int n)
+		{
+			foreach (var x in a)
+			{
+				Console.Write($"{x}\t");
+			}
+			Console.WriteLine();
+		}
+
+		/// <summary>
+		/// Sinh ngau nhien n so ghi vao mang a
+		/// </summary>
+		/// <param name="a"></param>
+		/// <param name="n"></param>
+		private static void Gen(int[] a, int n)
+		{
+			Random random = new Random();
+			if (random.Next(2) == 0)
+			{
+				// 1/2 so test la vo nghiem
+				for (int i = 0; i < n; i++)
+					a[i] = random.Next(n);
+				return;
+			}
+			int d = random.Next(n / 2) + 1; // Diem chia
+			int t = 0;
+			// Sinh doan a[0..d-1]
+			for (int i = 0; i < d; i++)
+			{
+				a[i] = random.Next(n);
+				t += a[i];
+			}
+			int n1 = n - 1;
+			// Sinh doan a[d..n-1]
+			for (int i = d; i < n1; i++)
+			{
+				a[i] = random.Next(t);
+				t -= a[i];
+			}
+			a[n - 1] = t; // Phan tu cuoi
+		}
+	}
+}
---
+++ src/Tap1/Chuong1_GiaiMotBaiToanTin/Bai1.5_ChiaMangTyLe1_1/README.md
@@ -0,0 +1,53 @@
+﻿# Chia mảng tỷ lệ 1:1
+
+*Tìm cách chia dãy số nguyên không âm a1, a2,...,an, n > 1 cho trước thành hai đoạn có tổng các phần tử trong mỗi đoạn bằng nhau.*
+
+## Đặc tả
+
+Ta quy ước viết #E là "tồn tại" và #V là "với mọi". Kí hiệu `sum(a[d..c])` là tổng các phần tử liên tiếp nhau từ a[d] đến a[c] của dãy a.
+
+```c#
+sum(a[d..c]) = a[d] + a[d+1] + ... + a[c]
+```
+
+Gọi t là tổng của các phần tử của mảng: `t = sum(a[1..n])`
+
+Muốn chia a thành hai đoạn a[1..i] và a[i+1..n] có tổng bằng nhau, ta phả có:
+
+(1) t là số chẵn (t chia hết cho 2). Đặt t2 = t div 2
+
+(2) (#E i: 1 <= i <= n): sum(a[1..i]) = t2
+
+## Chương trình
+
+Hàm `chia` cho giá trị i nếu mảng a được chia thành a[1..i] và a[i+1..n]. Trong trường hợp vô nghiệm `chia = -1`.
+Ta gọi i là điểm chia và dùng biến tr (tổng riêng) để tích lũy tổng các phần tử của đoạn đang xét a[1..i]. Khi tr = t2 thì bài toán có nghiệm i. Ngược lại, khi tr > t2 thì bài toán vô nghiệm.
+
+Ta khởi trị ngẫu nhiên cho mảng a. Tuy nhiên ta muốn số lần có nghiệm (mảng a chia được thành hai phần có tổng bằng nhau) xấp xỉ bằng số lần vô nghiệm. 
+Ta sẽ thực hiện mục tiêu đề ra như sau:
+
+Mỗi lần khởi trị ta tung đồng xu hai mặt. Nếu gặp mặt sấp (random(2)=0), ta sẽ khởi trị tùy ý cho mảng a, ngược lại, nếu gặp mặt ngửa (random(2)=1) ta khởi trị a là mảng có nghiệm.
+Để khởi trị sao cho mảng a có nghiệm ta lại chọn ngẫu nhiên một điểm cắt d trong khoảng 1..(n/2). Sau đó ta khởi trị ngẫu nhiên cho các phần tử a[1..d]. 
+Với các phần tử còn lại ta cũng khởi trị ngẫu nhiên trong khoảng hợp lí sao cho tổng các giá trị của chúng đúng bằng tổng t của đoạn a[1..d]. Bạn đọc xem chi tiết thủ tục Gen trong chương trình.
+
+## Kết quả
+
+```c#
+8       3       0       1       0       1       1       1       0       1
+
+Doan thu nhat: 1..1
+Doan thu hai: 2..10
+```
+
+```c#
+2       7       0       3       5       0       0       0       0       1
+
+Doan thu nhat: 1..2
+Doan thu hai: 3..10
+```
+
+```c#
+5       4       7       9       3       2       7       9       5       8
+
+Khong chia duoc
+```
