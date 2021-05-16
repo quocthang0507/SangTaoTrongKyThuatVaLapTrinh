11aadc566e42d7645467de0f4ca759655bce1564
La Quốc Thắng <quocthang0507@gmail.com>
2021-05-16 20:40:39 +0700

    Add 2.3 proj

---
+++ src/Tap1/Chuong2_SinhDuLieuVaoVaRa/Bai2.3_SinhHoanViNgauNhien/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Text;
+
+namespace Bai2._3_SinhHoanViNgauNhien
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.OutputEncoding = Encoding.UTF8;
+			while (true)
+			{
+				Console.WriteLine("Mảng hoán vị ngẫu nhiên 20 phần tử: ");
+				Print(Gen(20));
+				ConsoleKey input = Console.ReadKey().Key;
+				if (input != ConsoleKey.Enter)
+					break;
+			}
+		}
+
+		private static void Print(int[] a)
+		{
+			foreach (var x in a)
+			{
+				Console.Write(x + " ");
+			}
+			Console.WriteLine();
+		}
+
+		private static int[] Gen(int n)
+		{
+			Random random = new Random();
+			int[] a = new int[n];
+			for (int i = 0; i < n; i++)
+			{
+				a[i] = i + 1;
+			}
+			for (int i = 0; i < n; i++)
+			{
+				int j = random.Next(n);
+				int t = a[0];
+				a[0] = a[j];
+				a[j] = t;
+			}
+			return a;
+		}
+	}
+}
---
+++ src/Tap1/Chuong2_SinhDuLieuVaoVaRa/Bai2.3_SinhHoanViNgauNhien/README.md
@@ -0,0 +1,23 @@
+﻿# Sinh hoán vị ngẫu nhiên
+
+*Sinh ngẫu nhiên cho mảng nguyên a một hoán vị của 1..n*
+
+## Đặc tả
+
+Xuất phát từ hoán vị đơn vị a = (1, 2,..., n) ta đổi chỗ phần tử đầu tiên với một phần tử tùy ý
+a[j] ta sẽ được một hoán vị. Ta có thể thực hiện việc đổi chỗ nhiều lần.
+
+## Kết quả ví dụ
+
+```
+Mảng hoán vị ngẫu nhiên 20 phần tử:
+3 2 1 4 5 6 7 17 9 10 20 18 13 14 12 11 19 8 16 15
+Mảng hoán vị ngẫu nhiên 20 phần tử:
+12 6 9 17 5 15 7 8 10 19 11 4 13 16 2 3 14 1 18 20
+Mảng hoán vị ngẫu nhiên 20 phần tử:
+12 2 15 4 7 9 18 6 11 10 8 19 3 14 20 16 17 13 1 5
+Mảng hoán vị ngẫu nhiên 20 phần tử:
+18 7 3 9 12 10 4 6 17 1 11 2 13 14 8 16 15 5 19 20
+Mảng hoán vị ngẫu nhiên 20 phần tử:
+5 13 10 3 19 6 7 8 9 1 18 12 17 14 15 11 20 16 4 2
+```
