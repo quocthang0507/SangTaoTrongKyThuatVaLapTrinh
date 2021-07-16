af475163b5b78da34523baae45508ab7ef6a7c00
quocthang0507 <quocthang_0507@yahoo.com.vn>
2021-07-16 09:11:22 +0700

    Add new project

---
+++ src/Tap1/Chuong2_SinhDuLieuVaoVaRa/Bai2.6_SinhNgauNhienTepTang/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+namespace Bai2._6_SinhNgauNhienTepTang
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string filename = "Data.txt";
+			GenToFile(filename, 20);
+			Show(filename);
+			Console.ReadKey();
+		}
+
+		static void Show(string filename)
+		{
+			Console.WriteLine(File.ReadAllText(filename));
+		}
+
+		static void GenToFile(string filename, int n)
+		{
+			StreamWriter writer = new StreamWriter(filename);
+			Random random = new Random();
+			int x = random.Next(10);
+			for (int i = 0; i < n; i++)
+			{
+				writer.Write(x + " ");
+				// Moi dong 20 so
+				if (i % 20 == 19)
+					writer.WriteLine();
+				x += random.Next(10);
+			}
+			if (n % 20 != 19)
+				writer.WriteLine();
+			writer.Close();
+		}
+	}
+}
---
+++ src/Tap1/Chuong2_SinhDuLieuVaoVaRa/Bai2.6_SinhNgauNhienTepTang/README.md
@@ -0,0 +1,9 @@
+﻿# Sinh ngẫu nhiên tệp tăng
+
+*Sinh ngẫu nhiên n số tự nhiên sắp tăng và ghi vào một tệp văn bản có tên cho trước.*
+
+## Ví dụ kết quả
+
+```
+3 3 3 3 8 13 13 16 23 25 25 26 33 35 37 39 46 47 56 57
+```
