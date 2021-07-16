3cf1f256c6d29e8f9c3a570f723fe64d6219aa6b
quocthang0507 <quocthang_0507@yahoo.com.vn>
2021-07-16 09:15:26 +0700

    Add new project

---
+++ src/Tap1/Chuong2_SinhDuLieuVaoVaRa/Bai2.7_SinhNgauNhienTepCapSoCong/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+namespace Bai2._7_SinhNgauNhienTepCapSoCong
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string filename = "Data.txt";
+			GenToFile(filename, 50);
+			Show(filename);
+			Console.ReadLine();
+		}
+
+		static void Show(string fn)
+		{
+			Console.WriteLine(File.ReadAllText(fn));
+		}
+
+		static void GenToFile(string filename, int n)
+		{
+			StreamWriter writer = File.CreateText(filename);
+			Random random = new Random();
+			int s = random.Next(n), d = random.Next(10) + 1;
+			for (int i = 0; i < n; ++i)
+			{
+				writer.Write(s + " ");
+				if (i % 20 == 19) writer.WriteLine();
+				s += d;
+			}
+			if (n % 20 != 19) writer.WriteLine();
+			writer.Close();
+		}
+	}
+}
---
+++ src/Tap1/Chuong2_SinhDuLieuVaoVaRa/Bai2.7_SinhNgauNhienTepCapSoCong/README.md
@@ -0,0 +1,17 @@
+﻿# Sinh ngẫu nhiên tệp cấp số cộng
+
+*Sinh ngẫu nhiên một cấp số cộng có n số hạng và ghi vào tệp văn bản có tên cho trước.*
+
+## Đặc tả
+
+1. Sinh ngẫu nhiên số hạng thứ nhất và công sai d
+2. Sinh các phần tử a[i] tiếp theo theo cấp số cộng
+3. Ghi vào tệp
+
+## Ví dụ kết quả
+
+```
+21 24 27 30 33 36 39 42 45 48 51 54 57 60 63 66 69 72 75 78
+81 84 87 90 93 96 99 102 105 108 111 114 117 120 123 126 129 132 135 138
+141 144 147 150 153 156 159 162 165 168
+```
