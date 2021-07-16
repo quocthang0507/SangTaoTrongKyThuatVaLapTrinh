1cef5dfbf905ad4663b0502642692bb223020e23
quocthang0507 <quocthang_0507@yahoo.com.vn>
2021-07-16 09:02:37 +0700

    Add new project

---
+++ src/Tap1/Chuong2_SinhDuLieuVaoVaRa/Bai2.5_SinhNgauNhienTyLe/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace Bai2._5_SinhNgauNhienTyLe
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Random random = new Random();
+			int n, k;
+			do
+			{
+				Console.WriteLine(new string('=', 50));
+				n = random.Next(30) + 2;
+				k = random.Next(8) + 1;
+				Console.WriteLine($"n = {n}, k = {k}");
+				int[] a = new int[n];
+				int n1 = Gen(a, n, k);
+				Print(a);
+				Test(a, n1, k);
+				Console.Write("\nBam RETURN de tiep tuc: ");
+			} while (Console.ReadLine() == "");
+		}
+
+		static void Print(int[] a)
+		{
+			Console.WriteLine();
+			foreach (var x in a)
+			{
+				Console.Write(x + " ");
+			}
+			Console.WriteLine();
+		}
+
+		static int Gen(int[] a, int n, int k)
+		{
+			Random random = new Random();
+			int i = 0; // phan tu thu i trong a
+			int n1 = random.Next(n / 2) + 1; // n1 - so phan tu trong doan 1
+			int t1 = 0; // tong doan 1
+			for (; i < n1; i++)
+			{
+				a[i] = random.Next(10);
+				t1 += a[i];
+			}
+			int t2 = k * t1;
+			int tt = t1;
+			// Xac dinh ngau nhien
+			// 0. t2 gap k lan t1, hoac
+			// 1. t1 gap k lan t2
+			if (random.Next(2) == 1)
+			{
+				// t1 gap k lan t2
+				t1 = t2;
+				t2 = tt;
+				a[i - 1] += t1 - t2;
+			}
+			// Sinh doan 2
+			for (; i < n; i++)
+			{
+				a[i] = random.Next(t2);
+				t2 -= a[i];
+			}
+			a[n - 1] += t2;
+			return n1;
+		}
+
+		static void Test(int[] a, int n1, int k)
+		{
+			int t1 = 0;
+			for (int i = 0; i < n1; i++)
+			{
+				t1 += a[i];
+			}
+			Console.WriteLine($"\nDoan thu nhat: sum(a[0..{n1 - 1}]) = {t1}");
+			int t2 = 0;
+			for (int i = n1; i < a.Length; i++)
+			{
+				t2 += a[i];
+			}
+			Console.WriteLine($"\nDoan thu hai: sum(a[{n1}..{a.Length - 1}]) = {t2}");
+			if ((t1 == k * t2) || (t2 == k * t1))
+			{
+				Console.WriteLine("\nDUNG");
+			}
+			else
+			{
+				Console.WriteLine("\n SAI");
+			}
+		}
+	}
+}
---
+++ src/Tap1/Chuong2_SinhDuLieuVaoVaRa/Bai2.5_SinhNgauNhienTyLe/README.md
@@ -0,0 +1,55 @@
+﻿# Sinh ngẫu nhiên tỷ lệ
+
+*Sinh ngẫu nhiên cho mảng nguyên a có n phần tử tạo thành
+hai đoạn liên tiếp có tổng các phần tử trong một đoạn gấp
+k lần tổng các phần tử của đoạn kia.*
+
+## Đặc tả
+
+1. Sinh ngẫu nhiên tổng `t1 := random(n) + 1`
+2. Tính `t2 := k * t1`
+3. Gieo đồng xu bằng cách gọi `random(2)` để xác định tổng nào trong số t1 và t2 được chọn trước.
+4. Sinh `random(n div 2) + 1` phần tử cho đoạn thứ nhất sao cho
+tổng các phần tử của đoạn này bằng `t1`.
+5. Sinh nốt các phần tử cho đoạn thứ hai sao cho tổng các bằng tử của đoạn này bằng `t2`.
+
+## Ví dụ kết quả
+
+```
+==================================================
+n = 18, k = 2
+
+6 1 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1
+
+Doan thu nhat: sum(a[0..0]) = 6
+
+Doan thu hai: sum(a[1..17]) = 3
+
+DUNG
+
+Bam RETURN de tiep tuc:
+==================================================
+n = 16, k = 2
+
+2 8 3 1 0 0 0 0 0 0 0 0 0 0 0 1
+
+Doan thu nhat: sum(a[0..1]) = 10
+
+Doan thu hai: sum(a[2..15]) = 5
+
+DUNG
+
+Bam RETURN de tiep tuc:
+==================================================
+n = 24, k = 1
+
+1 3 2 1 0 0 4 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1
+
+Doan thu nhat: sum(a[0..2]) = 6
+
+Doan thu hai: sum(a[3..23]) = 6
+
+DUNG
+
+Bam RETURN de tiep tuc:
+```
