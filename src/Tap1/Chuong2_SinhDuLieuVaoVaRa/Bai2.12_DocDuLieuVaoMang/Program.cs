102b752f9ba0c5cc12c325adf311682645eafde3
quocthang0507 <quocthang_0507@yahoo.com.vn>
2021-08-20 20:19:15 +0700

    Add new project

---
+++ src/Tap1/Chuong2_SinhDuLieuVaoVaRa/Bai2.12_DocDuLieuVaoMang/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+
+namespace Bai2._12_DocDuLieuVaoMang
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string filename = "Input.txt";
+			int n = 0, m = 0;
+			int[,] a = Read(filename, ref n, ref m);
+			Print(a, n, m);
+			Console.ReadKey();
+		}
+
+		static int[,] Read(string filename, ref int n, ref int m)
+		{
+			if (!File.Exists(filename))
+				return null;
+			char[] cc = new char[] { ' ', '\n', '\t', '\r' };
+			string[] ss = File.ReadAllText(filename).Split(cc, StringSplitOptions.RemoveEmptyEntries);
+			int[] c = Array.ConvertAll(ss, new Converter<string, int>(int.Parse));
+			n = c[0];
+			m = c[1];
+			int[,] a = new int[n, m];
+			int k = 2;
+			for (int i = 0; i < n; i++)
+			{
+				for (int j = 0; j < m; j++)
+				{
+					a[i, j] = c[k++];
+				}
+			}
+			return a;
+		}
+
+		static void Print(int[,] a, int n, int m)
+		{
+			Console.WriteLine($"Mang a chieu cao n = {n}, chieu rong m = {m}:");
+			for (int i = 0; i < n; i++)
+			{
+				for (int j = 0; j < m; j++)
+				{
+					Console.Write($"{a[i, j]}\t");
+				}
+				Console.WriteLine();
+			}
+		}
+	}
+}
